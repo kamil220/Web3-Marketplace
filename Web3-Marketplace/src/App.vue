35dd7116ca201cf477b5fb38537a56fea1f2429d
Kamil Łazarz <kamillazarz@gmail.com>
2022-02-14 09:10:58 +0100

    Add Marketplace and Userinfo interface

--- Web3-Marketplace/src/App.vue
+++ Web3-Marketplace/src/App.vue
@@ -2,13 +2,21 @@
 import Navbar from "./components/Navbar.vue";
 import Quest from "./components/Quest.vue";
 import Marketplace from "./components/Marketplace.vue";
+import UserInfo from "./components/UserInfo.vue";
 </script>
 <template>
   <Navbar class="mx-auto container" :connected="this.connected" :tokenId="this.address" :tokens="this.tokens" @connect="this.connect"></Navbar>
 
   <div class="mx-auto container my-14">
     <Quest v-if="!this.connected"></Quest>
-    <Marketplace v-else></Marketplace>
+    <div v-else class="grid grid-cols-3 gap-2">
+      <div class="col-span-2">
+        <Marketplace @buyTokens="this.buyTokens"></Marketplace>
+      </div>
+      <div>
+        <UserInfo :tokens="this.tokens" :vestingBalance="this.vestingBalance" ></UserInfo>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,7 +32,9 @@ export default {
     return {
       connected: false,
       address: '',
-      tokens: 0
+      tokens: 0,
+      vestingBalance: 0,
+      vestingDate: false
     }
   },
   setup() {
@@ -56,9 +66,24 @@ export default {
 
     async refreshData() {
       let tokens = await this.token.balanceOf( this.address );
+      let vestingBalance = await this.token.vestingBalance();
+      let vestingRelease = await this.token.vestingDate( this.address );
 
       this.tokens = ethers.utils.formatUnits( tokens, 18 );
-    }
+      this.vestingBalance = ethers.utils.formatUnits( vestingBalance, 18 );
+    },
+
+    async buyTokens() {
+      let coins = document.querySelector( '#buy-leo-amount' )?.value;
+      coins = coins / 100;
+      try {
+        const response = await this.token.buyTokens({value: ethers.utils.parseEther( coins.toString() ) } );
+        response.wait();
+        await this.refreshData();
+      } catch ( e ) {
+        alert( e.data.message );
+      }
+    },
   }
 }
 
---
+++ Web3-Marketplace/src/components/UserInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="rounded-2xl w-full px-4 py-4">
+    <h3 class="text-2xl mb-4">Account balance</h3>
+    <label class="w-full block">
+      <span class="font-semibold">Total:</span> {{ this.tokens }}
+    </label>
+    <label class="w-full block">
+      <span class="font-semibold">Free:</span> {{ this.tokens - this.vestingBalance }}
+    </label>
+    <label class="w-full block">
+      <span class="font-semibold">Temporary blocked:</span> {{ this.vestingBalance }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserInfo",
+  props: {
+    tokens: Number,
+    vestingBalance: Number
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
